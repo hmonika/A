755cdae7093d01e52b27aa1e2f43807f2d557d3a
hmonika <hmonika@zeomega.com>
2014-03-05 19:40:05 +0530

    this is my first commit`

---
+++ eulers1.py
@@ -0,0 +1,10 @@
+def multiply():
+	sum=0
+	for i in range(1,1000,1):
+		if((i%3==0)or(i%5==0)):
+			sum+=i
+	print 'sum of all the multiples of 3 or 5 below 1000=',sum
+			
+
+if __name__=='__main__':
+	multiply()
